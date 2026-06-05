1e22191d0831535527f316c51297b95e46acbde5
Stackly Files <lokeshravi1818@gmail.com>
2026-06-05 07:32:50 +0530

    Added HTML and Javascript Files

---
+++ script.js
@@ -0,0 +1,77 @@
+// Task 1
+
+var studentName = "Lokesh";
+var studentAge = 18;
+var studentCourse = "MERN Stack";
+console.log(studentName);
+console.log(studentAge);
+console.log(studentCourse);
+
+// Task 2
+
+var company = "Stackly";
+let salary = 20000;
+const country = "India";
+
+company = "Google";
+salary = 30000;
+console.log(company);
+console.log(salary);
+console.log(country);
+
+// Task 3
+
+var userName = prompt("What is your name?");
+console.log("Welcome " + userName);
+
+
+// Task 4
+
+var userWish = confirm("Do you know javascript?");
+console.log(userWish);
+
+
+// Task 5
+var studentName = prompt("Enter Student Name:");
+var studentAge = prompt("Enter Student Age:");
+var studentCity = prompt("Enter Student City:");
+
+console.log("Name : " + studentName);
+console.log("Age : " + studentAge);
+console.log("City : " + studentCity);
+
+// Task 6
+let course = "Javascript";
+alert("Welcome to " + course);
+
+// Task 7
+document.writeln("Hello Student");
+document.writeln("<br>");
+document.writeln("Welcome to JavaScript");
+
+// Task 8
+var favouriteFood = "Chicken Briyani";
+var favouriteColor = "Blue";
+var favouriteMovie = "The Gravity";
+console.log("Food :" + favouriteFood);
+console.log("Color :" + favouriteColor);
+console.log("Movie :" + favouriteMovie);
+
+// Task 9
+var name = "Lokesh";
+var age = 25;
+var phone = 7397569458;
+var email = "lokeshr@thestackly.com"
+console.log("Name : " + name);
+console.log("Age : " + age);
+console.log("Phone : " + phone);
+console.log("Email : " + email);
+
+
+// Task 10
+
+var name = prompt("Enter your name:");
+var studentCourse = prompt("Enter your course:");
+console.log("Name : " + name);
+console.log("Course : " + studentCourse);
+alert("Hello " + name + ", Welcome to " + course + " Course");
